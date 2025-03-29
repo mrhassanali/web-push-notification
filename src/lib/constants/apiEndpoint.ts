787c148b138108d618413bc77f3603de4c0e8438
Hassan Ali <hassanalidotpk@gmail.com>
2025-03-29 06:49:42 +0500

    Adding Endpoint in Constants

---
+++ src/lib/constants/apiEndpoint.ts
@@ -0,0 +1,3 @@
+export const API_BASE_URL = "/api"
+export const WEB_PUSH_NOTIFICATION_API = `${API_BASE_URL}/web-push-notification`
+export const BROADCAST_NOTIFICATION_API = `${API_BASE_URL}/broadcast-notification`
